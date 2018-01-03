8670a3abfdb1e201f07a79005616a01efed6de8f
trronaldbasco <31196851+trronaldbasco@users.noreply.github.com>
2018-01-03 17:10:01 +0800

    homepage ui revision

--- webdesign/src/components/Home.vue
+++ webdesign/src/components/Home.vue
@@ -1,21 +1,20 @@
 <template lang="pug">
   section.landing.section.is-paddingless
-    div.bg-container
-        img.landing-bg(src="../assets/img/landing-page-bg.png")
-        .content-container.is-overlay
-            .container
-                .level 
-                    .level-item.direction-column
-                        .heading-container
-                            h1.title.is-1.is-lowercase
-                                | Lorem ipsum dolor, 
-                                br
-                                |   consectetur adipiscing,
-                                br
-                                |   incididunt 
-                            div.log-in-container
-                                a(href="#0", title="title").button.is-primary.is-uppercase  Client Log in
-                                a(href="#0", title="title").button.is-secondary.is-uppercase   Tech Log in
+    .content-container
+        .container
+            .level.is-mobile 
+                .level-item.direction-column
+                    .heading-container
+                        h1.title.is-1.is-lowercase
+                            | Lorem ipsum dolor, consectetur.
+                        p
+                            | Lorem ipsum dolor sit amet, consectetur  incididunt
+                            br
+                            | adipiscing elit, sed do eiusmod tempor
+
+                        div.log-in-container
+                            a(href="#0", title="title").button.is-primary.is-uppercase  Client Log in
+                            a(href="#0", title="title").button.is-secondary.is-uppercase   Tech Log in
 </template>
 
 <script>
